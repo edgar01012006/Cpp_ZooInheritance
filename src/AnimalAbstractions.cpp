0affe85b44f43075ecf9266389bfadb4a97bbdb0
edgar01012006 <edgarsalardzorcyan2006@gmail.com>
2025-11-01 18:18:59 +0400

    added source folder

--- src/AnimalAbstractions.cpp
+++ src/AnimalAbstractions.cpp
@@ -1,17 +1,20 @@
-#include "include/AnimalAbstractions.hpp"
-#include "include/Animals.hpp"
+#include "AnimalAbstractions.hpp"
+#include "Animals.hpp"
 
 int Animal::nextId = 0;
 
-Animal::Animal(const std::string& name, const Kind& kind)
+Animal::Animal(const std::string& name, Kind kind)
     : m_id { ++nextId }, m_name { name }, m_health { 100 }, m_hunger { 0 }, m_kind { kind } {}
 
 void Animal::print() const {
     switch (m_kind) {
         case Kind::Lion: static_cast<const Lion&>(*this).printInfo(); return;
         case Kind::Tiger: static_cast<const Tiger&>(*this).printInfo(); return;
+        case Kind::Elephant: static_cast<const Elephant&>(*this).printInfo(); return;
         case Kind::Eagle: static_cast<const Eagle&>(*this).printInfo(); return;
+        case Kind::Parrot: static_cast<const Parrot&>(*this).printInfo(); return;
         case Kind::Snake: static_cast<const Snake&>(*this).printInfo(); return;
+        case Kind::Crocodile: static_cast<const Crocodile&>(*this).printInfo(); return;
         default: break;
     }
 }
@@ -19,8 +22,8 @@ void Animal::print() const {
 void Animal::printInfo() const {
     std::cout << "Id: " << m_id << "\n";
     std::cout << "Name: " << m_name << "\n";
-    std::cout << "Health: " << m_health << "\n";
-    std::cout << "Hunger: " << m_hunger << "\n";
+    //std::cout << "Health: " << m_health << "\n";
+    //std::cout << "Hunger: " << m_hunger << "\n";
 } 
 
 /*void Animal::feed() {
